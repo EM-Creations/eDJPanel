f8cb9317b5a51af65cef5b92a7b129d96ec00f87
EM-Creations <eddy@em-creations.co.uk>
2021-10-24 19:43:04 +0100

    Update: Error handling for if the application is restarted twice.

--- Backend/eDJPanel/src/main/java/uk/co/emcreations/eDJPanel/EDjPanelApplication.java
+++ Backend/eDJPanel/src/main/java/uk/co/emcreations/eDJPanel/EDjPanelApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import lombok.extern.log4j.Log4j2;
+import uk.co.emcreations.eDJPanel.controller.HealthCheckController;
+
 @SpringBootApplication
+@Log4j2
 public class EDjPanelApplication {
 	private static ConfigurableApplicationContext context;
 
@@ -14,15 +18,20 @@ public class EDjPanelApplication {
 	}
 	
 	public static void restart() {
-        ApplicationArguments args = context.getBean(ApplicationArguments.class);
-
-        Thread thread = new Thread(() -> {
-            context.close();
-            context = SpringApplication.run(EDjPanelApplication.class, args.getSourceArgs());
-        });
-
-        thread.setDaemon(false);
-        thread.start();
+		if (null == context) {
+			log.error("Only 1 restart of the application is supported at this time, please manually restart the application if required.");
+			System.exit(0);
+		} else {
+	        ApplicationArguments args = context.getBean(ApplicationArguments.class);
+	
+	        Thread thread = new Thread(() -> {
+	            context.close();
+	            context = SpringApplication.run(EDjPanelApplication.class, args.getSourceArgs());
+	        });
+	
+	        thread.setDaemon(false);
+	        thread.start();
+		}
     }
 
 }
