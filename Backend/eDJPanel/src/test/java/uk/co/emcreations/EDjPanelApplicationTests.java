4f9687894dd96a9b0c05ff68233745419fdd0799
Edward McKnight <eddy@em-creations.co.uk>
2021-11-01 22:19:00 +0000

    Update: Tests made runnable.

--- Backend/eDJPanel/src/test/java/uk/co/emcreations/EDjPanelApplicationTests.java
+++ Backend/eDJPanel/src/test/java/uk/co/emcreations/EDjPanelApplicationTests.java
@@ -1,13 +1,23 @@
 package uk.co.emcreations;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import uk.co.emcreations.eDJPanel.EDjPanelApplication;
+import uk.co.emcreations.eDJPanel.controller.HealthCheckController;
+
+@SpringBootTest(classes = EDjPanelApplication.class)
 class EDjPanelApplicationTests {
 
+	@Autowired
+	private HealthCheckController healthCheckController;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(healthCheckController).isNotNull();
 	}
 
 }
